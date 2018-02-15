71190b76f88153b142fd006f345defb65e21a13d
Victor TENNERONI <vtennero@student.42.fr>
2018-02-15 17:31:25 +0100

    David Sarif

--- get_next_line.c
+++ get_next_line.c
@@ -12,70 +12,50 @@
 
 #include "get_next_line.h"
 
-static int		ft_eol_is_full(char *tmp, char **line)
-{
-	int		n;
-	char	*str;
-
-	str = *line;
-	n = -1;
-	if (tmp)
-	{
-		if ((n = ft_char_pos(tmp, '\n')) >= 0)
-		{
-			*line = ft_strndup(tmp, n);
-			free(str);
-			return (1);
-		}
-	}
-	return (0);
-}
-
-static char		*ft_read_line(char *t, char **line, int const fd, int *ret)
-{
-	int			n;
-	char		buf[BUFF_SIZE + 1];
-	char		*str;
-
-	str = NULL;
-	while ((*ret = read(fd, buf, BUFF_SIZE)) > 0)
-	{
-		buf[*ret] = '\0';
-		str = (!str) ? ft_strjoin_clr(t, buf, 3) : ft_strjoin_clr(str, buf, 0);
-		if ((n = ft_char_pos(str, '\n')) >= 0)
-		{
-			*line = ft_strndup(str, n);
-			t = ft_strdup(ft_strchr(str, '\n') + 1);
-			return (t);
-		}
-		else
-			*line = str;
-	}
-	return (t);
-}
-
 int				get_next_line(int const fd, char **line)
 {
-	static char	*t;
-	char		*tmp;
+	static char	buf[BUFF_SIZE + 1];
+	int			eol_pos;
 	int			ret;
 
 	if (!line || fd < 0)
 		return (-1);
 	*line = NULL;
-	tmp = NULL;
-	if (ft_eol_is_full(t, line) == 1)
+	eol_pos = ft_char_pos(buf, '\n');
+	if (ft_strlen(buf) > 0)
 	{
-		t = ft_strdup(ft_strchr(t, '\n') + 1);
-		return (1);
+		if ((eol_pos = ft_char_pos(buf, '\n')) >= 0)
+		{
+			*line = ft_strndup(buf, eol_pos);
+			ft_memcpy(buf, buf + eol_pos + 1, BUFF_SIZE);
+			return (1);
+		}
+		else
+		{
+			*line = ft_strdup(buf);
+			ft_bzero(buf, BUFF_SIZE);
+		}
 	}
-	tmp = ft_read_line(t, line, fd, &ret);
-	if (ft_strcmp(t, tmp) != 0)
+	if (eol_pos == -1)
 	{
-		t = tmp;
-		return (1);
+		while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+		{
+			buf[ret] = '\0';
+			if ((eol_pos = ft_char_pos(buf, '\n')) >= 0)
+			{
+				*line = ft_strjoin_clr(*line, ft_strndup(buf, eol_pos), 2);
+				ft_memcpy(buf, buf + eol_pos + 1, BUFF_SIZE);
+				return (1);
+			}
+			else
+			{
+				*line = ft_strjoin_clr(*line, buf, 0);
+				ft_bzero(buf, BUFF_SIZE);
+			}
+		}
 	}
-	if (ret == -1)
-		return (-1);
-	return (*line == NULL) ? 0 : 1;
+	if (*line)
+		return (1);
+	else
+		return (0);
 }
